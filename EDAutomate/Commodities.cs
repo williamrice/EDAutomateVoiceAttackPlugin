7ee6c2994581bce8617269c7f75d48d5e267425d
William Rice <william.rice192@gmail.com>
2020-09-10 21:06:06 -0400

    added Commodities to track possible commodities to search

---
+++ EDAutomate/Commodities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAutomate
+{
+    class Commodities
+    {
+        public enum Commodity
+        {
+            ParseError = 0,
+            VoidOpal = 10250,
+            Painite = 84,
+            LTD = 144
+        }
+    }
+}
