874c5f3412b8bb9b7be9b58131918d87998516e6
William Rice <william.rice192@gmail.com>
2020-09-20 23:03:40 -0400

    Version update to 0.0.0.7

--- EDAutomate/Utilities/Constants.cs
+++ EDAutomate/Utilities/Constants.cs
@@ -2,7 +2,7 @@ namespace EDAutomate.Utilities
 {
     public static class Constants
     {
-        public static readonly string DisplayName = "Ed Automate Plugin - V0.0.0.6alpha";
+        public static readonly string DisplayName = "Ed Automate Plugin - V0.0.0.7alpha";
         public static readonly string DisplayInfo = "Ed Automate automates input on popular third party Elite Dangerous programs as well as some built in voice commands for OVR Drop";
         public static readonly string DefaultLastKnownSystem = "sol";
         public static readonly string DefaultEliteDangerousJournalPath = @"Saved Games\Frontier Developments\Elite Dangerous";
